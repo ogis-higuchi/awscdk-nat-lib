119316f726c9eb88e1f3bbd77ed40134502948a4
Masatoshi Higuchi <2480630+ogis-higuchi@users.noreply.github.com>
2024-10-04 07:02:49 +0000

    chore(github): add contributor statement to upgrade workflow

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,4 +1,7 @@
 import { awscdk, javascript } from 'projen';
+import { Job } from 'projen/lib/github/workflows-model';
+
+const contributorStatement = 'By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license.';
 
 const project = new awscdk.AwsCdkConstructLibrary({
   name: '@ogis-rd/awscdk-nat-lib',
@@ -11,7 +14,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   githubOptions: {
     pullRequestLintOptions: {
-      contributorStatement: 'By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license.',
+      contributorStatement,
       semanticTitleOptions: {
         types: [
           'chore',
@@ -45,5 +48,12 @@ const project = new awscdk.AwsCdkConstructLibrary({
   authorAddress: 'https://www.ogis-ri.co.jp',
   authorOrganization: true,
 });
+
+// upgrade-main workflow's 'pr' job
+const upgradeMainPrJob = project.upgradeWorkflow!.workflows[0].getJob('pr') as Job;
+// Add contributor statement.
+// This is a workaround for linting the PR created by human action (workflow_dispatch).
+// When we enable the workflow to be triggered automatically, not manually, this workaround would be deleted.
+upgradeMainPrJob.steps[4].with!.body += `\n${contributorStatement}`;
 
 project.synth();
